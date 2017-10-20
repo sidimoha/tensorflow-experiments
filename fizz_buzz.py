1df983e903524b58cbba2dbfe7527bdf0994de11
Idriss SIDI MOHAMED <sidimoha@protonmail.com>
2017-10-21 00:33:34 +0200

    Adds a 2nd hidden layer, decreases training iteration, cleans code

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -6,16 +6,18 @@ import tensorflow as tf
 
 NUM_DIGITS = 10
 
-# Represent each input by an array of its binary digits.
-
 
 def binary_encode(i, num_digits):
+    """
+    Represent each input by an array of its binary digits.
+    """
     return np.array([i >> d & 1 for d in range(num_digits)])
-
-# One-hot encode the desired outputs: [number, "fizz", "buzz", "fizzbuzz"]
 
 
 def fizz_buzz_encode(i):
+    """
+    encode the desired outputs: [number, "fizz", "buzz", "fizzbuzz"]
+    """
     if i % 15 == 0:
         return np.array([0, 0, 0, 1])
     elif i % 5 == 0:
@@ -26,35 +28,54 @@ def fizz_buzz_encode(i):
         return np.array([1, 0, 0, 0])
 
 
-# Our goal is to produce fizzbuzz for the numbers 1 to 100. So it would be
-# unfair to include these in our training data. Accordingly, the training data
-# corresponds to the numbers 101 to (2 ** NUM_DIGITS - 1).
-trX = np.array([binary_encode(i, NUM_DIGITS)
-                for i in range(1, 2 ** NUM_DIGITS)])
-trY = np.array([fizz_buzz_encode(i) for i in range(1, 2 ** NUM_DIGITS)])
-
-# We'll want to randomly initialize weights.
-
-
 def init_weights(shape):
     """
     Initialize the weight in a random fashion
     """
     return tf.Variable(tf.random_normal(shape, stddev=0.01))
 
-# Our model is a standard 1-hidden-layer multi-layer-perceptron with ReLU
-# activation. The softmax (which turns arbitrary real-valued outputs into
-# probabilities) gets applied in the cost function.
+
+def fizz_buzz(i, prediction):
+    """
+    turn a prediction (and an original number)
+    into a fizz buzz output
+    """
+    return [str(i), "fizz", "buzz", "fizzbuzz"][prediction]
 
 
 def model(input_layer, w_h, w_i, w_o):
     """
-    Create the Neural Network with 1 hidden layer (perceptron style) with ReLU activation function
+    Create the Neural Network with 2 hidden layer (perceptron style) with ReLU activation function
     """
     h = tf.nn.relu(tf.matmul(input_layer, w_h))
     h_i = tf.nn.relu(tf.matmul(h, w_i))
     return tf.matmul(h_i, w_o)
 
+
+def fizz_buzz_expected_answer(i):
+    """
+    Generate the correct fizz-buzz answer for an int
+    """
+    if i % 15 == 0:
+        return "fizzbuzz"
+    elif i % 5 == 0:
+        return "buzz"
+    elif i % 3 == 0:
+        return "fizz"
+    else:
+        return str(i)
+
+
+EXPECTED_OUTPUT = [
+    fizz_buzz_expected_answer(i) for i in range(1, 101)]
+
+
+# Our goal is to produce fizzbuzz for the numbers 1 to 100. So it would be
+# unfair to include these in our training data. Accordingly, the training data
+# corresponds to the numbers 101 to (2 ** NUM_DIGITS - 1).
+trX = np.array([binary_encode(i, NUM_DIGITS)
+                for i in range(1, 2 ** NUM_DIGITS)])
+trY = np.array([fizz_buzz_encode(i) for i in range(1, 2 ** NUM_DIGITS)])
 
 # Our variables. The input has width NUM_DIGITS, and the output has width 4.
 X = tf.placeholder("float", [None, NUM_DIGITS])
@@ -72,7 +93,7 @@ w_o = init_weights([NUM_HIDDEN, 4])
 py_x = model(X, w_h, w_i, w_o)
 
 # We'll train our model by minimizing a cost function.
-#cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(py_x, Y))
+# cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(py_x, Y))
 cost = tf.reduce_mean(
     tf.nn.softmax_cross_entropy_with_logits(logits=py_x, labels=Y))
 train_op = tf.train.GradientDescentOptimizer(0.05).minimize(cost)
@@ -80,22 +101,17 @@ train_op = tf.train.GradientDescentOptimizer(0.05).minimize(cost)
 # And we'll make predictions by choosing the largest output.
 predict_op = tf.argmax(py_x, 1)
 
-# Finally, we need a way to turn a prediction (and an original number)
-# into a fizz buzz output
-
-
-def fizz_buzz(i, prediction):
-    return [str(i), "fizz", "buzz", "fizzbuzz"][prediction]
-
-
 BATCH_SIZE = 128
+# 6500 with 1 hidden layer
+# 3000 with 2 hidden layers
+TRAINING_ITERATIONS = 3000
 
 # Launch the graph in a session
 with tf.Session() as sess:
     # tf.initialize_all_variables().run()
     sess.run(tf.global_variables_initializer())
 
-    for epoch in range(6500):
+    for epoch in range(TRAINING_ITERATIONS):
         # Shuffle the data before each training iteration.
         p = np.random.permutation(range(len(trX)))
         trX, trY = trX[p], trY[p]
@@ -111,9 +127,16 @@ with tf.Session() as sess:
         #                      sess.run(predict_op, feed_dict={X: trX, Y: trY})))
 
     # And now for some fizz buzz
-    numbers = np.arange(1, 101)
+    numbers = np.arange(1, 1001)
     teX = np.transpose(binary_encode(numbers, NUM_DIGITS))
     teY = sess.run(predict_op, feed_dict={X: teX})
     output = np.vectorize(fizz_buzz)(numbers, teY)
 
-    print(output)
+    print("result:")
+    for index, (expected, actual) in enumerate(zip(EXPECTED_OUTPUT, output)):
+        # print(index, "expected:", expected, "actual:", actual)
+        if expected != actual:
+            print("integer: {0: >4} {1: <6} {2: <9} {3: <8} {4: <9} {5: <8}".format(
+                index + 1, "  OK  " if expected == actual else " FAIL ", "expected:", expected, "actual:", actual))
+    else:
+        print("All OK")
